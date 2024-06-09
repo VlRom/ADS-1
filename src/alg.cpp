c0d09ceea2cfc3c7b6e01c166dc1ce276408e152
VladRomashkin <150885042+VlRom@users.noreply.github.com>
2024-06-09 23:09:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,10 +4,10 @@
 
 
 bool checkPrime(uint64_t value) {
-	if (value<=1){
+	if (value <= 1){
 		return false;
 	}
-	for (uint64_t i=2; i<=sqrt(value); i++) {
+	for (int i = 2; i <= sqrt(value); i++) {
 		if (value % i == 0) {
 			return false;
 		}
@@ -30,7 +30,7 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-	uint64_t pNumber = value+1;
+	uint64_t pNumber = value + 1;
 	while(!checkPrime(pNumber)){
 		pNumber++;
 	}
@@ -39,7 +39,7 @@ uint64_t nextPrime(uint64_t value) {
 
 uint64_t sumPrime(uint64_t hbound) {
 	uint64_t summa = 0;
-	for (uint64_t i=2; i<hbound; i++) {
+	for (uint64_t i = 2; i < hbound; i++) {
 		if (checkPrime(i)) {
 			summa += i;
 		}
